12d9fbbbba55d53672b3ca74d1ff4777daacd3bb
Xinbin Lin <936348175@qq.com>
2016-12-30 20:04:57 +0800

    数据库脚本
    liubie

---
+++ resources/ddl/01_blog_create_table.sql
@@ -0,0 +1,177 @@
+/*
+SQLyog Ultimate v11.24 (32 bit)
+MySQL - 5.6.11 : Database - lxb_test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `sys_dictionaries` */
+
+DROP TABLE IF EXISTS `sys_dictionaries`;
+
+CREATE TABLE `sys_dictionaries` (
+  `DICTIONARIES_ID` varchar(100) NOT NULL,
+  `NAME` varchar(30) DEFAULT NULL COMMENT '简体中文',
+  `NAME_TW` varchar(30) DEFAULT NULL COMMENT '繁体中文',
+  `NAME_EN` varchar(50) DEFAULT NULL COMMENT '英文',
+  `NAME_JA` varchar(50) DEFAULT NULL COMMENT '日文',
+  `NAME_MY` varchar(50) DEFAULT NULL COMMENT '马来文',
+  `BIANMA` varchar(50) DEFAULT NULL COMMENT '编码',
+  `ORDER_BY` int(11) DEFAULT NULL COMMENT '排序',
+  `PARENT_ID` varchar(100) DEFAULT NULL COMMENT '上级ID',
+  `BZ` varchar(255) DEFAULT NULL COMMENT '备注',
+  `ICON` text COMMENT '图标路径',
+  `TBSNAME` varchar(100) DEFAULT NULL COMMENT '排查表',
+  `STATE` varchar(2) DEFAULT NULL,
+  `BAK1` varchar(30) DEFAULT NULL COMMENT '扩展字段,对于产品和产品分类来说标识是否为热门.0-是,1-否',
+  PRIMARY KEY (`DICTIONARIES_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `sys_fhbutton` */
+
+DROP TABLE IF EXISTS `sys_fhbutton`;
+
+CREATE TABLE `sys_fhbutton` (
+  `FHBUTTON_ID` varchar(100) NOT NULL,
+  `NAME` varchar(30) DEFAULT NULL COMMENT '名称',
+  `QX_NAME` varchar(50) DEFAULT NULL COMMENT '权限标识',
+  `BZ` varchar(255) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`FHBUTTON_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `sys_log` */
+
+DROP TABLE IF EXISTS `sys_log`;
+
+CREATE TABLE `sys_log` (
+  `id` varchar(32) NOT NULL,
+  `ip` varchar(128) DEFAULT NULL COMMENT '访问者的IP',
+  `request_path` text COMMENT '访问路径',
+  `start_date` varchar(24) DEFAULT NULL COMMENT '开始时间',
+  `end_date` varchar(24) DEFAULT NULL COMMENT '结束时间',
+  `result` text COMMENT '处理结果',
+  `user_id` varchar(32) DEFAULT NULL COMMENT '访问者的后台登录ID',
+  `method` varchar(10) DEFAULT NULL COMMENT '请求方式',
+  `user_agent` varchar(400) DEFAULT NULL COMMENT '请求头的USERAGENT信息',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+/*Table structure for table `sys_menu` */
+
+DROP TABLE IF EXISTS `sys_menu`;
+
+CREATE TABLE `sys_menu` (
+  `MENU_ID` int(11) NOT NULL,
+  `MENU_NAME` varchar(255) DEFAULT NULL,
+  `MENU_URL` varchar(255) DEFAULT NULL,
+  `PARENT_ID` varchar(100) DEFAULT NULL,
+  `MENU_ORDER` varchar(100) DEFAULT NULL,
+  `MENU_ICON` varchar(60) DEFAULT NULL,
+  `MENU_TYPE` varchar(10) DEFAULT NULL,
+  `MENU_STATE` int(1) DEFAULT NULL,
+  PRIMARY KEY (`MENU_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `sys_param` */
+
+DROP TABLE IF EXISTS `sys_param`;
+
+CREATE TABLE `sys_param` (
+  `PARAM_ID` varchar(100) NOT NULL,
+  `NAME` varchar(20) DEFAULT NULL COMMENT '参数名',
+  `VAL` varchar(1000) DEFAULT NULL COMMENT '参数值',
+  `BZ` varchar(255) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`PARAM_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `sys_role` */
+
+DROP TABLE IF EXISTS `sys_role`;
+
+CREATE TABLE `sys_role` (
+  `ROLE_ID` varchar(100) NOT NULL,
+  `ROLE_NAME` varchar(100) DEFAULT NULL,
+  `RIGHTS` varchar(255) DEFAULT NULL,
+  `PARENT_ID` varchar(100) DEFAULT NULL,
+  `ADD_QX` varchar(255) DEFAULT NULL,
+  `DEL_QX` varchar(255) DEFAULT NULL,
+  `EDIT_QX` varchar(255) DEFAULT NULL,
+  `CHA_QX` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ROLE_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `sys_role_fhbutton` */
+
+DROP TABLE IF EXISTS `sys_role_fhbutton`;
+
+CREATE TABLE `sys_role_fhbutton` (
+  `RB_ID` varchar(100) NOT NULL,
+  `ROLE_ID` varchar(100) DEFAULT NULL,
+  `BUTTON_ID` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`RB_ID`),
+  KEY `角色表外键` (`ROLE_ID`) USING BTREE,
+  KEY `fbutton` (`BUTTON_ID`),
+  CONSTRAINT `fbutton` FOREIGN KEY (`BUTTON_ID`) REFERENCES `sys_fhbutton` (`FHBUTTON_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `frole` FOREIGN KEY (`ROLE_ID`) REFERENCES `sys_role` (`ROLE_ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `sys_user` */
+
+DROP TABLE IF EXISTS `sys_user`;
+
+CREATE TABLE `sys_user` (
+  `USER_ID` varchar(100) NOT NULL,
+  `USERNAME` varchar(255) DEFAULT NULL,
+  `PASSWORD` varchar(255) DEFAULT NULL,
+  `NAME` varchar(255) DEFAULT NULL,
+  `RIGHTS` varchar(255) DEFAULT NULL,
+  `ROLE_ID` varchar(100) DEFAULT NULL,
+  `LAST_LOGIN` varchar(255) DEFAULT NULL,
+  `IP` varchar(100) DEFAULT NULL,
+  `STATUS` varchar(32) DEFAULT NULL,
+  `BZ` varchar(255) DEFAULT NULL,
+  `SKIN` varchar(100) DEFAULT NULL,
+  `EMAIL` varchar(32) DEFAULT NULL,
+  `NUMBER` varchar(100) DEFAULT NULL,
+  `PHONE` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`USER_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `t_docrecord` */
+
+DROP TABLE IF EXISTS `t_docrecord`;
+
+CREATE TABLE `t_docrecord` (
+  `id` int(12) NOT NULL AUTO_INCREMENT,
+  `filename` varchar(200) DEFAULT NULL,
+  `doctype` varchar(10) DEFAULT NULL,
+  `lastmodify` bigint(13) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `tb_pictures` */
+
+DROP TABLE IF EXISTS `tb_pictures`;
+
+CREATE TABLE `tb_pictures` (
+  `PICTURES_ID` varchar(100) NOT NULL,
+  `TITLE` varchar(255) DEFAULT NULL COMMENT '标题',
+  `NAME` varchar(255) DEFAULT NULL COMMENT '文件名',
+  `PATH` varchar(255) DEFAULT NULL COMMENT '路径',
+  `CREATETIME` varchar(100) DEFAULT NULL COMMENT '创建时间',
+  `MASTER_ID` varchar(255) DEFAULT NULL COMMENT '属于',
+  `BZ` varchar(255) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`PICTURES_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ resources/ddl/02_blog_init_data.sql
@@ -0,0 +1,52 @@
+/*
+SQLyog Ultimate v11.24 (32 bit)
+MySQL - 5.6.11 : Database - lxb_test
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Data for the table `sys_dictionaries` */
+
+/*Data for the table `sys_fhbutton` */
+
+insert  into `sys_fhbutton`(`FHBUTTON_ID`,`NAME`,`QX_NAME`,`BZ`) values ('46992ea280ba4b72b29dedb0d4bc0106','发邮件','email','发送电子邮件');
+
+/*Data for the table `sys_log` */
+
+/*Data for the table `sys_menu` */
+
+insert  into `sys_menu`(`MENU_ID`,`MENU_NAME`,`MENU_URL`,`PARENT_ID`,`MENU_ORDER`,`MENU_ICON`,`MENU_TYPE`,`MENU_STATE`) values (1,'系统管理','#','0','10','menu-icon fa fa-desktop blue','2',1),(2,'权限管理','#','1','1','menu-icon fa fa-lock black','1',1),(36,'角色(基础权限)','role.do','2','1','menu-icon fa fa-key orange','1',1),(37,'按钮权限','buttonrights/list.do','2','2','menu-icon fa fa-key green','1',1),(38,'菜单管理','menu/listAllMenu.do','1','3','menu-icon fa fa-folder-open-o brown','1',1),(39,'按钮管理','fhbutton/list.do','1','2','menu-icon fa fa-download orange','1',1),(40,'用户管理','#','0','7','menu-icon fa fa-users blue','2',1),(41,'系统用户','user/listUsers.do','40','1','menu-icon fa fa-users green','1',1),(43,'数据字典','dictionaries/listAllDict.do?DICTIONARIES_ID=0','1','4','menu-icon fa fa-book purple','1',1),(61,'测试菜单bbj','bbj/list.do','22','2','menu-icon fa fa-leaf black','1',1),(100,'参数项','param/list.do','1','4','menu-icon fa fa-leaf black','1',1);
+
+/*Data for the table `sys_param` */
+
+insert  into `sys_param`(`PARAM_ID`,`NAME`,`VAL`,`BZ`) values ('9fb3fbee66fe468d858f454b24bc9348','RES_DIR','E:/res/','静态资源存放目录'),('caab9d9c7b7b4b02833fe77333a4f803','API_SECRET','$t%rGvFb','服务端密钥，用于服务器和app端的通讯校验');
+
+/*Data for the table `sys_role` */
+
+insert  into `sys_role`(`ROLE_ID`,`ROLE_NAME`,`RIGHTS`,`PARENT_ID`,`ADD_QX`,`DEL_QX`,`EDIT_QX`,`CHA_QX`) values ('1','系统管理组','5923600852152800150339472076830291552886726','0','1','1','1','1'),('115b386ff04f4352b060dffcd2b5d1da','中级会员','498','2','0','0','0','0'),('1b67fc82ce89457a8347ae53e43a347e','初级会员','498','2','0','0','0','0'),('2','会员组','498','0','0','0','0','1'),('3264c8e83d0248bb9e3ea6195b4c0216','系统管理员','3802951800684692725613204603078','1','2745757475961053486176818902302049547153563590','2748566294888769542605944182694910380361113542','510423577002070300284756946605316235206','22300705290411743380241273568078190912462790'),('8b70a7e67f2841e7aaba8a4d92e5ff6f','高级会员','498','2','0','0','0','0');
+
+/*Data for the table `sys_role_fhbutton` */
+
+insert  into `sys_role_fhbutton`(`RB_ID`,`ROLE_ID`,`BUTTON_ID`) values ('d55968621ca24c229fa5e1d8c932d6e9','3264c8e83d0248bb9e3ea6195b4c0216','46992ea280ba4b72b29dedb0d4bc0106');
+
+/*Data for the table `sys_user` */
+
+insert  into `sys_user`(`USER_ID`,`USERNAME`,`PASSWORD`,`NAME`,`RIGHTS`,`ROLE_ID`,`LAST_LOGIN`,`IP`,`STATUS`,`BZ`,`SKIN`,`EMAIL`,`NUMBER`,`PHONE`) values ('098cc6af197b43888bda3a6b50f9b4db','linxinbin','846af8e5d35f419b376d7a57b26cd3734bdfc152','林鑫滨','','3264c8e83d0248bb9e3ea6195b4c0216','2016-10-31 12:31:05','0:0:0:0:0:0:0:1','0','哈哈','default','936348175@qq.com','linxinbin','13726516869'),('1','admin','de41b7fb99201d8334c23c014db35ecd92df81bc','FH','1133671055321055258374707980945218933803269864762743594642571294','3264c8e83d0248bb9e3ea6195b4c0216','2016-12-23 16:27:12','0:0:0:0:0:0:0:1','0','admin','default','QQ313596790@main.com','001','18788888888'),('b0e764cf06084e66ac718b243ca14f38','zouqianyu','b9d0373ef6ed70e35c1c56bd04119874065d5c14','邹乾宇','','3264c8e83d0248bb9e3ea6195b4c0216','2016-11-04 10:14:34','0:0:0:0:0:0:0:1','0','','default','15695@qq.com','611','18064032748');
+
+/*Data for the table `t_docrecord` */
+
+/*Data for the table `tb_pictures` */
+
+insert  into `tb_pictures`(`PICTURES_ID`,`TITLE`,`NAME`,`PATH`,`CREATETIME`,`MASTER_ID`,`BZ`) values ('1727eb943b2941a49762c1c30bb4e99c','图片','b2b36ea7dbfd4fb4a8ccf6d6ddc09fd1.png','','2016-12-20 09:48:12','1','图片管理处上传'),('b1a8ac7d9e47409a9243e012309affc2','图片','cef0ec1afe88409badb2337f40d07295.gif','20161028/cef0ec1afe88409badb2337f40d07295.gif','2016-10-28 10:10:51','1','图片管理处上传'),('d8ef572b9633445fbc51aeaa585801e7','图片','55e91513ad0d4c81823b5800231faee9.png','20161220/55e91513ad0d4c81823b5800231faee9.png','2016-12-20 09:48:12','1','图片管理处上传');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
